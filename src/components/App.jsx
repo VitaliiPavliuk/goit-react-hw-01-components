581ca966b5aa5006259e2ebfcf574dbc418967da
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-02-19 17:16:16 +0200

    Add Profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import { Profile } from './Profile/Profile';
+import user from '../data/user';
+
 export const App = () => {
   return (
     <div
@@ -7,10 +10,10 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <Profile profile={user} />
     </div>
   );
 };
