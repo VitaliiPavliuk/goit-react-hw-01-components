028e252840bd2cf70cf10c33bd1e2eece10e89ea
Vitalii Pavliuk <pavlyuk.vitaliy@gmail.com>
2023-02-19 19:10:09 +0200

    Add FriendList component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,10 @@
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
+import { FriendList } from './FriendList/FriendList';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
+import friends from '../data/friends.json';
 
 export const App = () => {
   return (
@@ -20,6 +22,8 @@ export const App = () => {
 
       <Statistics title="Upload stats" stats={data} />
       {/* <Statistics stats={data} /> */}
+
+      <FriendList friends={friends} />
     </div>
   );
 };
---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+
+import { FriendListItem } from './FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul class="friend-list">
+      {friends.map(friend => (
+        <li class="item" key={friend.id}>
+          <FriendListItem friend={friend} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired }).isRequired
+  ).isRequired,
+};
